fc0e33e4d123fb202409ff7ad8e718d9c2472a47
Ayder <muzdabaev252@gmail.com>
2026-03-06 13:48:56 +0300

    test task / gym_analysis

---
+++ src/main/kotlin/lesson20/analysis.kt
@@ -0,0 +1,18 @@
+package lesson20
+
+data class Set(
+    val name: String,
+    val reps: Int,
+    val weight: Double,
+)
+
+fun main() {
+    val workoutResults = listOf<Set>(
+        Set("Жим",4,80.0),
+        Set("Жим",3,90.0),
+        Set("Жим",2,120.0),
+        Set("Тяга блока",8,65.0),
+        Set("Отведения гантелей",36,10.0)
+    )
+    workoutResults.filter { it.weight>50 }.forEach {println("Тяжелый подход: ${it.name}, вес: ${it.weight}")}
+}

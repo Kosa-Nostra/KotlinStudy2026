6c084b13d46240d47808a3b5b6f56ff15c744e71
Ayder <muzdabaev252@gmail.com>
2026-03-23 17:09:37 +0300

    final_learning / lambdas 2

--- src/main/kotlin/final_learning/lambdas/lambdas_main.kt
+++ src/main/kotlin/final_learning/lambdas/lambdas_main.kt
@@ -2,8 +2,13 @@ package final_learning.lambdas
 
 fun main() {
     saySomething("Ayder"){println(it)}
-
+    saySomething("Ayder"){
+        name -> println("Тут : ${name.length} букв")
+    }
+    saySomething("Ayder"){
+        name -> println("Наоборот будет: ${name.reversed()}")
+    }
 }
-fun saySomething(name: String, speech: (String)->Unit){
+fun saySomething(name: String, speech: (String)->Unit){//speech - это какое-то действие
     speech(name)
 }
